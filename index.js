384e83a1e0e14fdc6a557dc1df3c4884fb583224
donell <donell.wambua@student.moringaschool.com>
2023-06-14 20:25:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=31 
+let num2=2
+let multiply =num1 * num2
 
-// Write your code here
+
+let random = (Math.ceil (Math.random() *10)+1)
+
+let num3=15
+let num4=14
+let mod=num3% num4
+
+let max=Math.max(15,10,12,14,17,20,7,9)
